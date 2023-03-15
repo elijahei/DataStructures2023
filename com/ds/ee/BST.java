c30446dff35ac733aa7eeb5dfefc7f996b6fe072
elijahei <einsteine98@gmail.com>
2023-03-15 17:58:44 -0400

    serialization

--- com/ds/ee/BST.java
+++ com/ds/ee/BST.java
@@ -2,15 +2,16 @@ package com.ds.ee;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.io.*;
 
 
 /*
  * A Binary Search Tree that stores ints, this tree does not store duplicates
  */
 
- public class BST {
+ public class BST implements java.io.Serializable{
 
-    class BSTNode{ // inner class
+    class BSTNode implements java.io.Serializable{ // inner class
         int val;
         BSTNode right;
         BSTNode left;
@@ -102,6 +103,7 @@ import java.util.ArrayList;
 
     public List<Integer> levelorderTraversal(){
         //TODO: implement this 
+
         return null;
     }
     
@@ -110,20 +112,52 @@ import java.util.ArrayList;
     public static void main(String[] args) {
        
         
-        BST b = new BST(5);
-        b.insert(10);
-        b.insert(8);
-        b.insert(5);
-        b.insert(6);
+        // BST b = new BST(5);
+        // b.insert(10);
+        // b.insert(8);
+        // b.insert(5);
+        // b.insert(6);
 
-        b.insert(9);
-        b.insert(56);
-        b.insert(45);
-        b.insert(60);
+        // b.insert(9);
+        // b.insert(56);
+        // b.insert(45);
+        // b.insert(60);
 
         
-       List<Integer> inorderList = b.inorder();
-       System.out.println(inorderList);
+        // try {
+        //     FileOutputStream fileOut =
+        //     new FileOutputStream("/tmp/BST.ser");
+        //     ObjectOutputStream out = new ObjectOutputStream(fileOut);
+        //     out.writeObject(b);
+        //     out.close();
+        //     fileOut.close();
+        //     System.out.printf("Serialized data is saved in /tmp/BST.ser");
+        //  } catch (IOException i) {
+        //     i.printStackTrace();
+        //  }
+
+        BST bs = null;
+
+        try {
+            FileInputStream fileIn = new FileInputStream("/tmp/BST.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            bs = (BST) in.readObject();
+            in.close();
+            fileIn.close();
+         } catch (IOException i) {
+            i.printStackTrace();
+            return;
+         } catch (ClassNotFoundException c) {
+            System.out.println("Employee class not found");
+            c.printStackTrace();
+            return;
+         }
+
+         List<Integer> il = bs.inorder();
+
+         System.out.println("size is " + bs.size());
+         System.out.println(il);
+
 
     }
 
