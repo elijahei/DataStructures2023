8c7ec3e67751f8959a6043622766611daf1eaaa5
elijahei <einsteine98@gmail.com>
2023-03-15 13:26:09 -0400

    working on it

--- com/ds/ee/BST.java
+++ com/ds/ee/BST.java
@@ -1,34 +1,88 @@
 package com.ds.ee;
 
+import java.util.List;
+import java.util.ArrayList;
+
 
 /*
  * A Binary Search Tree of type T
  */
 
- class BST<T> {
+ public class BST {
 
     class BSTNode{ // inner class
-        T val;
+        int val;
         BSTNode right;
         BSTNode left;
-        BSTNode(T val){
+        BSTNode(int val){
             this.val = val;
             this.right = null;
             this.left = null;
         }
     }
 
-    BSTNode root;
-    int size;
+    private BSTNode root;
+    private int size;
     
     public BST(){
         this.root = null;
         this.size = 0;
     }
 
-    public BST(T val){
+    public BST(int val){
         this.root = new BSTNode(val);
         this.size = 1;
+    }
+
+
+    public int size(){
+        return this.size;
+    }
+
+    public boolean insert(int val){
+        if(this.root == null){
+            root = new BSTNode(val);
+            size += 1;
+            return true;
+        }
+        else{
+            if(this.root.val > val){
+                this.root.left = new BSTNode(val);
+                size+=1;
+                return true;
+            }
+            else if(this.root.val < val){
+                this.root.right = new BSTNode(val);
+                size+=1;
+                return true;
+            }
+            else{ // equal case
+                return false;
+            }
+        }
+       //return false;
+    }
+
+    public List<Integer> inorderTraversal(){
+        //TODO: implement this 
+        return null;
+    }
+
+    public List<Integer> levelorderTraversal(){
+        //TODO: implement this 
+        return null;
+    }
+    
+
+
+    public static void main(String[] args) {
+       
+        
+        BST b = new BST(5);
+        System.out.println(b.size());
+        b.insert(10);
+        System.out.println(b.size());
+
     }
 
 
--- com/ds/ee/Main.java
+++ com/ds/ee/Main.java
@@ -1,11 +1,13 @@
 package com.ds.ee;
+import com.ds.ee.BST;
 
 public class Main{
     public static void main(String[] args) {
         System.out.println("Hello World!");
         
-        BST<Integer> b = new BST<Integer>(5);
+        BST b = new BST(5);
         System.out.println(b.size);
 
     }
+
 }
