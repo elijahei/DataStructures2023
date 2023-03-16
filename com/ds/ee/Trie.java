430c42395d34243ad6b8bf0e2f6f411634ffd631
elijahei <einsteine98@gmail.com>
2023-03-16 13:56:15 -0400

    debugging comments added

--- com/ds/ee/Trie.java
+++ com/ds/ee/Trie.java
@@ -2,14 +2,22 @@ package com.ds.ee;
 
 import java.util.HashMap;
 
+/*.
+ * File: Trie.java
+ * Author: Elijah Einstein <einsteine98@gmail.com>
+ * 
+ */
+
+
+// TODO: debug -- checkString() returns false in some cases when its supposed to return true, example: insert star and starbucks, checkString("starbucks") is false
+
 public class Trie {
 
-
-
+   
     class TrieNode{
        
-        HashMap<Character,TrieNode> map;
-        boolean isValidString;
+        HashMap<Character,TrieNode> map; // contains current character, and a TrieNode to the next character in string
+        boolean isValidString; // is true if string till here is valid
         TrieNode(){
             map = new HashMap<Character,TrieNode>();
             isValidString = false;
@@ -20,30 +28,33 @@ public class Trie {
 
     public Trie(){
         this.node = new TrieNode();
-        // this.node.map = new HashMap<Character,TrieNode>();
-        // this.node.isValidString = false;
-        this.node.map.put('$', null);
         this.node.isValidString = false;
     }
 
+
+    // inserts s into this Trie
     public void insertString(String s){
         if(s.length() == 0)
             return;
-        TrieNode newNode = new TrieNode();
 
+        TrieNode newNode = new TrieNode();
         this.node.map.put(s.charAt(0), newNode);
+        System.out.println(s.charAt(0));
 
         for(int i = 1; i < s.length(); i++){
             TrieNode nNode = new TrieNode();
             newNode.map.put(s.charAt(i), nNode);
             newNode = nNode;
+            System.out.println(s.charAt(i));
         }
-        newNode.map.put('$', null);
+       
         newNode.isValidString = true;
+        System.out.println("--------------------------------------------------------");
     }
 
+
+    // returns true if s is in the Trie, false otherwise 
     public boolean checkString(String s){
-        //TODO: Implement this
 
         if(s.length() == 0){
             return false;
@@ -55,6 +66,7 @@ public class Trie {
        return checkStringHelper(s, this.node, 0);
     }
 
+    //helper method called by insertString()
     private boolean checkStringHelper(String s, TrieNode n, int index){
 
         TrieNode nn = null;
@@ -70,7 +82,13 @@ public class Trie {
 
 
         nn = n.map.get(s.charAt(index));
+        System.out.println(s.charAt(index));
+
         if(nn == null){
+            System.out.println(index);
+            System.out.println(n.map);
+            System.out.println(s.length());
+            System.out.println(s.charAt(index+1));
             return false;
         }
 
@@ -85,9 +103,11 @@ public class Trie {
         t.insertString("hell");
         t.insertString("samy");
         t.insertString("samosa");
+        t.insertString("starbucks");
+        t.insertString("star");
         //boolean flag = t.checkString("sammy");
 
-        System.out.println(t.checkString("samy"));
+        System.out.println(t.checkString("starbucks"));
     }
     
 }
