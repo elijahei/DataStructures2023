88392909a185dea7ad69487f3670bb8d15a0601a
elijahei <einsteine98@gmail.com>
2023-03-24 19:56:15 -0400

    just playing around

---
+++ Cpp/fun.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+
+    int x,y,z;
+
+    std::cin >>x >> y >> z;
+
+    std::cout << std::to_string(x) << " " << std::to_string(y) << " " << std::to_string(z) << std::endl;
+
+    return -1;
+
+}
