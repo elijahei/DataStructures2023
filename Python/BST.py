68cc454b924010cea9bcec1f75957fd7bbc29fa8
elijahei <einsteine98@gmail.com>
2023-04-03 23:43:26 -0400

    dev python actually lol

--- Python/BST.py
+++ Python/BST.py
@@ -1,5 +1,23 @@
 
 
+class BSTNode:
+
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right - right
+
+
+class BST:
+
+    def __init__(self):
+        self.root = None
+        self.size = 0
+    
+
+   
+
+
 
 def main():
     print('main function!')
