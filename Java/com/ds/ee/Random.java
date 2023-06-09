48062b9aca16393e2980052ab3ce15f42774e564
elijahei <einsteine98@gmail.com>
2023-06-08 20:33:30 -0400

    incorrect insertsort rn

--- Java/com/ds/ee/Random.java
+++ Java/com/ds/ee/Random.java
@@ -2,19 +2,30 @@
 class Random{
 
     public static void main(String[] args) {
-        int[] arr = {3,5,1,2};
-        insertSort(arr, 4);
+        int[] arr = {31,41,59,26,41,58};
+        insertSort(arr, arr.length);
         
-        for(int i = 0; i < 4; i++)
+        for(int i = 0; i < arr.length; i++)
             System.out.println(arr[i]);
 
 
     }
 
+    // TODO: Incorrect Algo - write correct algo
     public static void insertSort(int[] arr, int n){
 
-        for(int i = 0; i < n; i++){
-            arr[i] = 0;
+        for(int i = 1; i < n; i++){
+            int key = arr[i];
+            // Insert key into sorted position in arr[0:i-1]
+            int j = i-1;
+
+            while (j >= 0 && arr[j] > key){
+                j = j-1;
+            }
+            if(j >= 0){
+                arr[i] = arr[j];
+                arr[j] = key;
+            }
         }
     }
 }
